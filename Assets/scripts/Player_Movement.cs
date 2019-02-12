695dc262917d790ee843d99663ba97ae00317c2c
hunter <hults.hunter36@gmail.com>
2019-02-11 20:59:39 -0800

    Initial commit

---
+++ Assets/scripts/Player_Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+    
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
